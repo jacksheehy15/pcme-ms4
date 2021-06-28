83a3fe6833ddabbf9faee7b38e169f82d1938bbf
jacksheehy15 <sheehyboi@gmail.com>
2021-06-28 14:20:56 +0000

    Added Allauth

--- pc.me/asgi.py
+++ pc.me/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petrolheads.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pcme.settings')
 
 application = get_asgi_application()
--- pc.me/wsgi.py
+++ pc.me/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petrolheads.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pcme.settings')
 
 application = get_wsgi_application()
